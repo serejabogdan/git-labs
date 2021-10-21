12ce084b4ab5f8524e8f54b617bfd3463d80669d
Serhii Bohdan <serejabgdn@gmail.com>
2021-10-21 23:32:01 +0300

    jhgjhvjhvjhvjhvjhjhvjhvjhvjhvjvhjhv

---
+++ file.cpp
@@ -0,0 +1,18 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[ ])
+{
+QApplication app(argc, argv);
+
+QPushButton button("Hello, World!");
+button.resize(200, 60);
+button.show( );
+
+return app.exec( );
+}
+
+modified
+awdwakjdlhdwahdwahd
+awdwadwadawdawd
+iuhghhvkhbkjb
